d69f1274aa511087a8860babc42bc742d1990d65
admin1 <zhanjob90@gmail.com>
2023-06-07 01:47:46 +0300

    +sketch

---
+++ Blink/Blink.ino
@@ -0,0 +1,11 @@
+// Простой пример мигания светодиодом
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);                      
+}
